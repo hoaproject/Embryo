6b9f30c12e42de1bb799accaf069da69017fbd12
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2011-04-20 17:39:17 +0200

    Files from http://hg.hoa-project.net/Central.

---
+++ Data/Etc/Configuration/.Cache/HoaControllerRouter.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Generated the 2010-11-19T18:11:06.000000Z
+ */
+
+return array (
+  'keywords' => 
+  array (
+  ),
+  'parameters' => 
+  array (
+    'base' => '/',
+    'rewrited' => false,
+    'rules' => 
+    array (
+    ),
+  ),
+);
