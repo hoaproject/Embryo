39de389ce2b6daeb9f242e2696124206f900e9bd
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2012-10-10 09:07:45 +0200

    Use require_once instead of require.
    It allows to run index.php from another file that already uses Hoa.

--- Application/Public/index.php
+++ Application/Public/index.php
@@ -2,9 +2,9 @@
 
 namespace {
 
-require dirname(dirname(__DIR__)) .
-        DIRECTORY_SEPARATOR . 'Data' .
-        DIRECTORY_SEPARATOR . 'Core.link.php';
+require_once dirname(dirname(__DIR__)) .
+             DIRECTORY_SEPARATOR . 'Data' .
+             DIRECTORY_SEPARATOR . 'Core.link.php';
 
 echo 'Hello you! I\'m your first Hoa-based application \o/.' . "\n";
 
