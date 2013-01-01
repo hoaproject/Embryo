3ebac7a4534ad01c4efe56bfd10b4c22e33c1a31
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2013-01-01 13:51:33 +0100

    Happy new year \o/.

--- Stub/Phar.php
+++ Stub/Phar.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2012, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2013, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -60,14 +60,9 @@ class Filter extends \FilterIterator {
     }
 }
 
-$iterator = new \AppendIterator();
-$iterator->append(new \RecursiveIteratorIterator(
-    new \RecursiveDirectoryIterator($root . 'Application')
-));
-$iterator->append(new \RecursiveIteratorIterator(
-    new \RecursiveDirectoryIterator($root . 'Data')
-));
-
+$iterator = new \RecursiveIteratorIterator(
+    new \RecursiveDirectoryIterator($root)
+);
 $phar     = new \Phar(__DIR__ . DS . $name);
 $phar->setMetadata(array(
     'author'          => 'Ivan Enderlin',
